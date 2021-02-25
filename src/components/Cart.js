6fc6549466ed9cce1906debbcb231504a53d95e8
Sylvia <smora1992@gmail.com>
2021-02-25 18:12:35 +0400

    new changes

---
+++ src/components/Cart.js
@@ -0,0 +1,39 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+import { USCurrencyFormat } from "../constants";
+
+function Cart(props) {
+  const total = Object.keys(props.selected).reduce(
+    (acc, curr) => acc + props.selected[curr].cost,
+    0
+  );
+
+  const summary = Object.keys(props.selected).map((feature, idx) => {
+    const featureHash = feature + "-" + idx;
+    const selectedOption = props.selected[feature];
+
+    return (
+      <CartItem
+        feature={feature}
+        featureHash={featureHash}
+        selectedOption={selectedOption}
+      />
+    );
+  });
+
+  return (
+    <section className="main__summary">
+      <h2>Your cart</h2>
+      {summary}
+      <div className="summary__total">
+        <div className="summary__total__label">Total</div>
+        <div className="summary__total__value">
+          {USCurrencyFormat.format(total)}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Cart;
---
+++ src/components/Customize.js
@@ -0,0 +1,48 @@
+import React from "react";
+import slugify from "slugify";
+
+import { USCurrencyFormat } from "../constants";
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+
+function Customize(props) {
+  const features = Object.keys(props.features).map((feature, idx) => {
+    const featureHash = feature + "-" + idx;
+    const options = props.features[feature].map((item) => {
+      const itemHash = slugify(JSON.stringify(item));
+      return (
+        <div key={itemHash} className="feature__item">
+          <input
+            type="radio"
+            id={itemHash}
+            className="feature__option"
+            name={slugify(feature)}
+            checked={item.name === props.selected[feature].name}
+            onChange={(e) => props.updateFeature(feature, item)}
+          />
+          <label htmlFor={itemHash} className="feature__label">
+            {item.name} ({USCurrencyFormat.format(item.cost)})
+          </label>
+        </div>
+      );
+    });
+
+    return (
+      <fieldset className="feature" key={featureHash}>
+        <legend className="feature__name">
+          <h3>{feature}</h3>
+        </legend>
+        {options}
+      </fieldset>
+    );
+  });
+  return (
+    <form className="main__form">
+      <h2>Customize your laptop</h2>
+      {features}
+    </form>
+  );
+}
+
+export default Customize;
