fec806559ff83640d38b6980354500de0e1c3cf8
Sylvia <smora1992@gmail.com>
2021-03-02 17:29:28 +0400

    Updates

---
+++ precache-manifest.8ba8943854dfcc3d080bcbf906eb084e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe296d6d09e9168ddb06fb8345bb126d",
+    "url": "/laptop_customizer/index.html"
+  },
+  {
+    "revision": "9d4bb58eabe4442ea148",
+    "url": "/laptop_customizer/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/laptop_customizer/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "9d4bb58eabe4442ea148",
+    "url": "/laptop_customizer/static/js/main.520bdcae.chunk.js"
+  },
+  {
+    "revision": "54eb8c3b7e85a3776709",
+    "url": "/laptop_customizer/static/js/runtime~main.11bd55f9.js"
+  }
+]);
